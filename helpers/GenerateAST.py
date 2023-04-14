a24a79b484cf75ddcaa245729252accabebeff2d
Vyasa M Nayak <21d61.vyasa@sjec.ac.in>
2023-04-14 15:52:18 +0530

    Rewrote GenerateAST in Python

---
+++ helpers/GenerateAST.py
@@ -0,0 +1,75 @@
+from sys import exit, argv
+from os.path import join
+
+
+def main():
+    if len(argv) != 2:
+        exit("Usage: generate_ast.py <output directory>")
+
+    types = [
+        "Binary   : Expr left, Token operator, Expr right",
+        "Grouping : Expr expression",
+        "Literal  : Object value",
+        "Unary    : Token operator, Expr right",
+    ]
+
+    outputDir = argv[1]
+    defineAst(outputDir, "Expr", types)
+
+
+def defineAst(outputDir: str, baseName: str, types: list) -> str:
+    with open(join(outputDir, baseName + ".java"), "w") as codeFile:
+
+        # Headers
+        codeFile.write("package com.craftinginterpreters.lox;\n")
+        codeFile.write("\n")
+        codeFile.write("import java.util.List;\n")
+        codeFile.write("\n")
+        codeFile.write("abstract class " + baseName + " {\n\n")
+
+        # Visitor Interface
+        codeFile.write("    interface Visitor<R> {\n")
+
+        # Visitor types
+        for typ in types:
+            typeName = typ.split(":")[0].strip()
+            codeFile.write(f"       R visit{typeName}{baseName}({typeName} {baseName.lower()});\n")
+
+        codeFile.write("    }\n\n")
+
+        # Types
+        for typ in types:
+            className = typ.split(":")[0].strip()
+            fieldList = typ.split(":")[1].strip()
+
+            # Subclass
+            codeFile.write(f"    static class {className} extends {baseName} " + "{\n")
+            codeFile.write(f"        {className} ({fieldList}) " + "{\n")
+
+            # Constructor
+            fields = fieldList.split(", ")
+            for field in fields:
+                name = field.split(" ")[1]
+                codeFile.write(f"            this.{name} = {name};\n")
+
+            codeFile.write("        }\n\n")
+
+            # Accept for visitor
+            codeFile.write("        @Override\n")
+            codeFile.write("        <R> R accept(Visitor<R> visitor) {\n")
+            codeFile.write(f"            return visitor.visit{className}{baseName}(this);\n")
+            codeFile.write("        }\n\n")
+
+            # Fields
+            for field in fields:
+                codeFile.write(f"        final {field};\n")
+
+            codeFile.write("    }\n\n")
+
+        # Base accept method
+        codeFile.write("    abstract<R> R accept(Visitor<R> visitor);\n")
+        codeFile.write("}")
+
+
+if __name__ == "__main__":
+    main()
--- jlox/com/craftinginterpreters/lox/Expr.java
+++ jlox/com/craftinginterpreters/lox/Expr.java
@@ -1,68 +1,73 @@
 package com.craftinginterpreters.lox;
 
-// import java.util.List;
+import java.util.List;
 
 abstract class Expr {
-  interface Visitor<R> {
-    R visitBinaryExpr(Binary expr);
-    R visitGroupingExpr(Grouping expr);
-    R visitLiteralExpr(Literal expr);
-    R visitUnaryExpr(Unary expr);
-  }
-  static class Binary extends Expr {
-    Binary(Expr left, Token operator, Expr right) {
-      this.left = left;
-      this.operator = operator;
-      this.right = right;
+
+    interface Visitor<R> {
+       R visitBinaryExpr(Binary expr);
+       R visitGroupingExpr(Grouping expr);
+       R visitLiteralExpr(Literal expr);
+       R visitUnaryExpr(Unary expr);
     }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-      return visitor.visitBinaryExpr(this);
+    static class Binary extends Expr {
+        Binary (Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitBinaryExpr(this);
+        }
+
+        final Expr left;
+        final Token operator;
+        final Expr right;
     }
 
-    final Expr left;
-    final Token operator;
-    final Expr right;
-  }
-  static class Grouping extends Expr {
-    Grouping(Expr expression) {
-      this.expression = expression;
+    static class Grouping extends Expr {
+        Grouping (Expr expression) {
+            this.expression = expression;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitGroupingExpr(this);
+        }
+
+        final Expr expression;
     }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-      return visitor.visitGroupingExpr(this);
+    static class Literal extends Expr {
+        Literal (Object value) {
+            this.value = value;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLiteralExpr(this);
+        }
+
+        final Object value;
     }
 
-    final Expr expression;
-  }
-  static class Literal extends Expr {
-    Literal(Object value) {
-      this.value = value;
+    static class Unary extends Expr {
+        Unary (Token operator, Expr right) {
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitUnaryExpr(this);
+        }
+
+        final Token operator;
+        final Expr right;
     }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-      return visitor.visitLiteralExpr(this);
-    }
-
-    final Object value;
-  }
-  static class Unary extends Expr {
-    Unary(Token operator, Expr right) {
-      this.operator = operator;
-      this.right = right;
-    }
-
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-      return visitor.visitUnaryExpr(this);
-    }
-
-    final Token operator;
-    final Expr right;
-  }
-
-  abstract <R> R accept(Visitor<R> visitor);
+    abstract<R> R accept(Visitor<R> visitor);
 }
