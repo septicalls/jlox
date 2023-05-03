75222349af5857390d9f357ba9b92c9389747390
Vyasa M Nayak <21d61.vyasa@sjec.ac.in>
2023-05-03 15:36:02 +0530

    Added a ZeroDivision Error

--- jlox/com/craftinginterpreters/lox/Interpreter.java
+++ jlox/com/craftinginterpreters/lox/Interpreter.java
@@ -81,6 +81,9 @@ class Interpreter implements Expr.Visitor<Object> {
             throw new RuntimeError(expr.operator, "Operands must be two numbers or strings.");
         case SLASH:
             checkNumberOperands(expr.operator, left, right);
+            if ((double)right == 0) {
+                throw new RuntimeError(expr.operator, "Division by Zero is not allowed.");
+            }
             return (double)left / (double)right;
         case STAR:
             checkNumberOperands(expr.operator, left, right);
