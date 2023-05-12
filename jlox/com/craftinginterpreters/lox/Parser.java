4d2758b046a62f68c557c56773180cc5fed62ad8
Vyasa M Nayak <21d61.vyasa@sjec.ac.in>
2023-05-12 09:39:24 +0530

    Fixed print Stmt bug

--- jlox/com/craftinginterpreters/lox/Parser.java
+++ jlox/com/craftinginterpreters/lox/Parser.java
@@ -48,7 +48,7 @@ class Parser {
     private Stmt printStatement() {
         Expr value = expression();
         consume(SEMICOLON, "Expect ';' after value.");
-        return new Stmt.Expression(value);
+        return new Stmt.Print(value);
     }
 
       private Stmt varDeclaration() {
