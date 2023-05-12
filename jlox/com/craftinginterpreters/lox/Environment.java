705bbad220d30b15f2d87ffd40cadda95ff369a6
Vyasa M Nayak <21d61.vyasa@sjec.ac.in>
2023-05-12 16:17:50 +0530

    nil variable access causes runtime error

--- jlox/com/craftinginterpreters/lox/Environment.java
+++ jlox/com/craftinginterpreters/lox/Environment.java
@@ -21,7 +21,12 @@ class Environment {
 
     Object get(Token name) {
         if (values.containsKey(name.lexeme)) {
-            return values.get(name.lexeme);
+            Object value = values.get(name.lexeme);
+            if (value == null) {
+                throw new RuntimeError(name,
+                    "Uninitialized variable accessed '" + name.lexeme + "'.");
+            }
+            return value;
         }
 
         if (enclosing != null) return enclosing.get(name);
