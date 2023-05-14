6a57e4b942a3b9092728a7b658362c4ee8710649
Vyasa M Nayak <21d61.vyasa@sjec.ac.in>
2023-05-14 18:12:21 +0530

    Logical operators covered

--- helpers/GenerateAST.py
+++ helpers/GenerateAST.py
@@ -11,6 +11,7 @@ def main():
         "Binary   : Expr left, Token operator, Expr right",
         "Grouping : Expr expression",
         "Literal  : Object value",
+        "Logical  : Expr left, Token operator, Expr right",
         "Unary    : Token operator, Expr right",
         "Variable : Token name",
     ]
