375ad29b27ed3c4e546b12ed52c6b11f79faac2b
Vyasa M Nayak <21d61.vyasa@sjec.ac.in>
2023-05-14 17:52:32 +0530

    Makes If statement classes

--- helpers/GenerateAST.py
+++ helpers/GenerateAST.py
@@ -18,6 +18,7 @@ def main():
     StmtTypes = [
         "Block      : List<Stmt> statements",
         "Expression : Expr expression",
+        "If         : Expr condition, Stmt thenBranch, Stmt elseBranch",
         "Print      : Expr expression",
         "Var        : Token name, Expr initializer",
     ]
