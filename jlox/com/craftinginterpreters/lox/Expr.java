9a0264ff84255721c4f8ef74e42bdb9eab5e66bb
septicalls <septicalls@proton.me>
2023-06-06 17:29:51 +0530

    Fixed misalighend whitespaces

--- jlox/com/craftinginterpreters/lox/Expr.java
+++ jlox/com/craftinginterpreters/lox/Expr.java
@@ -5,14 +5,14 @@ import java.util.List;
 abstract class Expr {
 
     interface Visitor<R> {
-       R visitAssignExpr(Assign expr);
-       R visitBinaryExpr(Binary expr);
-       R visitCallExpr(Call expr);
-       R visitGroupingExpr(Grouping expr);
-       R visitLiteralExpr(Literal expr);
-       R visitLogicalExpr(Logical expr);
-       R visitUnaryExpr(Unary expr);
-       R visitVariableExpr(Variable expr);
+        R visitAssignExpr(Assign expr);
+        R visitBinaryExpr(Binary expr);
+        R visitCallExpr(Call expr);
+        R visitGroupingExpr(Grouping expr);
+        R visitLiteralExpr(Literal expr);
+        R visitLogicalExpr(Logical expr);
+        R visitUnaryExpr(Unary expr);
+        R visitVariableExpr(Variable expr);
     }
 
     static class Assign extends Expr {
