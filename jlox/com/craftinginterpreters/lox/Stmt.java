f1c77ce4a15f300bd7a8365b4482a20e9258f157
septicalls <septicalls@proton.me>
2023-06-06 17:29:43 +0530

    Parsed Class declarations

--- jlox/com/craftinginterpreters/lox/Stmt.java
+++ jlox/com/craftinginterpreters/lox/Stmt.java
@@ -5,15 +5,16 @@ import java.util.List;
 abstract class Stmt {
 
     interface Visitor<R> {
-       R visitBlockStmt(Block stmt);
-       R visitBreakStmt(Break stmt);
-       R visitExpressionStmt(Expression stmt);
-       R visitFunctionStmt(Function stmt);
-       R visitIfStmt(If stmt);
-       R visitPrintStmt(Print stmt);
-       R visitReturnStmt(Return stmt);
-       R visitVarStmt(Var stmt);
-       R visitWhileStmt(While stmt);
+        R visitBlockStmt(Block stmt);
+        R visitBreakStmt(Break stmt);
+        R visitClassStmt(Class stmt);
+        R visitExpressionStmt(Expression stmt);
+        R visitFunctionStmt(Function stmt);
+        R visitIfStmt(If stmt);
+        R visitPrintStmt(Print stmt);
+        R visitReturnStmt(Return stmt);
+        R visitVarStmt(Var stmt);
+        R visitWhileStmt(While stmt);
     }
 
     static class Block extends Stmt {
@@ -40,6 +41,21 @@ abstract class Stmt {
         }
 
         final Token token;
+    }
+
+    static class Class extends Stmt {
+        Class (Token name, List<Stmt.Function> methods) {
+            this.name = name;
+            this.methods = methods;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitClassStmt(this);
+        }
+
+        final Token name;
+        final List<Stmt.Function> methods;
     }
 
     static class Expression extends Stmt {
