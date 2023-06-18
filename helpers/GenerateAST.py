a17d0e291662fe09839113ecbe26ac2926e1a153
septicalls <septicalls@proton.me>
2023-06-18 19:54:17 +0530

    Added AST node for superclasses

--- helpers/GenerateAST.py
+++ helpers/GenerateAST.py
@@ -18,6 +18,7 @@ def main():
         "Literal  : Object value",
         "Logical  : Expr left, Token operator, Expr right",
         "Set      : Expr object, Token name, Expr value",
+        "Super    : Token keyword, Token method",
         "This     : Token keyword",
         "Unary    : Token operator, Expr right",
         "Variable : Token name",
@@ -26,7 +27,7 @@ def main():
     stmt_types = [
         "Block      : List<Stmt> statements",
         "Break      : Token token",
-        "Class      : Token name, List<Stmt.Function> methods",
+        "Class      : Token name, Expr.Variable superclass, List<Stmt.Function> methods",
         "Expression : Expr expression",
         "Function   : Token name, List<Token> params, List<Stmt> body",
         "If         : Expr condition, Stmt thenBranch, Stmt elseBranch",
